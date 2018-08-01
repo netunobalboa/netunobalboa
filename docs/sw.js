410f2036d6bb67691b50348fe1401aeada5d8ef6
netunobalboa <41651276+netunobalboa@users.noreply.github.com>
2018-08-01 13:29:20 -0300

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -4,6 +4,8 @@
 const PRECACHE = 'precache-v6';
 const RUNTIME = 'runtime';
 
+console.log("versão do sw" + PREACHE);
+
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   './', // Alias for index.html
@@ -14,7 +16,7 @@ const PRECACHE_URLS = [
 // The install handler takes care of precaching the resources we always need.
 self.addEventListener('install', event => {
   
-  console.log("a");
+  console.log("entrou no install");
   event.waitUntil(
     caches.open(PRECACHE)
       .then(cache => cache.addAll(PRECACHE_URLS))
@@ -25,15 +27,16 @@ self.addEventListener('install', event => {
 // The activate handler takes care of cleaning up old caches.
 self.addEventListener('activate', event => {
   
-  console.log("b");
+  console.log("entrou no activate");
   const currentCaches = [PRECACHE, RUNTIME];
   event.waitUntil(
     caches.keys().then(cacheNames => {
       
       return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
     }).then(cachesToDelete => {
-     
+     console.log("entrou na limpeza de cache");
       return Promise.all(cachesToDelete.map(cacheToDelete => {
+        console.log("entrou na limpeza de cache 2");
         return caches.delete(cacheToDelete);
       }));
     }).then(() => self.clients.claim())
@@ -46,22 +49,25 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   
-  console.log("c");
+  console.log("entrou no fetch");
   // Skip cross-origin requests, like those for Google Analytics.
   if (event.request.url.startsWith(self.location.origin)) {
   
     event.respondWith(
       caches.match(event.request).then(cachedResponse => {
- //       alert("passo 2");
+        console.log("entrou no fetch 2");
         if (cachedResponse) {
-   //       alert("passo 3");
+          console.log("entrou no fetch 3");
           return cachedResponse;
         }
 
         return caches.open(RUNTIME).then(cache => {
+          console.log("entrou no fetch 4");
           return fetch(event.request).then(response => {
+            console.log("entrou no fetch 5");
             // Put a copy of the response in the runtime cache.
             return cache.put(event.request, response.clone()).then(() => {
+              console.log("entrou no fetch 6");
               return response;
             });
           });
