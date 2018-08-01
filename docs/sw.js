2d08cc38c767849e6670e5a1500c3e084207963f
netunobalboa <41651276+netunobalboa@users.noreply.github.com>
2018-08-01 12:37:46 -0300

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,14 +1,14 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v2';
+const PRECACHE = 'precache-v3';
 const RUNTIME = 'runtime';
 
 // A list of local resources we always want to be cached.
 const PRECACHE_URLS = [
   './', // Alias for index.html
   'styles.css',
-  'app.js'
+ // 'app.js'
 ];
 
 // The install handler takes care of precaching the resources we always need.
